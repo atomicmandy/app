129f5bf1c52095fa0d829c0169cce56f90a636e1
mandy <atomicmandy@gmail.com>
2024-09-30 09:11:07 +0300

    main

---
+++ app.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std ;
+
+int main(void){
+    cout << "your char is : ";
+    char c;
+    cin >> c ;
+    cout << " your ascii code is " << int(c);
+    return 0;
+}
